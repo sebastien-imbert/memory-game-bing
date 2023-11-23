1ba1b2287ddd0ffe3b57fbb31e3b5beae6144157
Sébastien Imbert <imbert.s@MacBook-Pro-de-Sebastien.local>
2023-11-23 10:26:18 +0100

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,144 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import ConfettiExplosion from 'react-confetti-explosion';
+import Logo from "../public/logo.svg";
+import { shuffle } from '../utils/shuffle';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  // memory game cards data
+  const data = [
+    { id: 3, name: "Papa", image: "/bandit.png", flipped: false, matched: false },
+    { id: 9, name: "Maman", image: "/chilli.png", flipped: false, matched: false },
+    { id: 7, name: "Bingo", image: "/bingo.png", flipped: false, matched: false },
+    { id: 2, name: "Papa", image: "/bandit.png", flipped: false, matched: false },
+    { id: 6, name: "Bluey", image: "/bluey.png", flipped: false, matched: false },
+    { id: 8, name: "Mamie", image: "/mamie.png", flipped: false, matched: false },
+    { id: 5, name: "Bingo", image: "/bingo.png", flipped: false, matched: false },
+    { id: 1, name: "Maman", image: "/chilli.png", flipped: false, matched: false },
+    { id: 10, name: "Papi", image: "/papi.png", flipped: false, matched: false },
+    { id: 11, name: "Bluey", image: "/bluey.png", flipped: false, matched: false },
+    { id: 4, name: "Papi", image: "/papi.png", flipped: false, matched: false },
+    { id: 12, name: "Mamie", image: "/mamie.png", flipped: false, matched: false },
+  ];
+
+  // const shuffledData = shuffle(data);
+
+  const maxMoves = 10;
+  const targertScore = data.length / 2;
+
+  const [cards, setCards] = useState<any[]>(data);
+  const [flipped, setFlipped] = useState<any[]>([]);
+  const [matched, setMatched] = useState<any[]>([]);
+  const [moves, setMoves] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [gameOver, setGameOver] = useState<boolean>(false);
+  const [canPlay, setCanPlay] = useState<boolean>(true);
+  const [isExploding, setIsExploding] = useState(false);
+
+  function handleClick(id: number) {
+    if (!canPlay) return;
+    const findCard = cards.find((card) => card.id === id);
+    if (!findCard.flipped) {
+      setFlipped([...flipped, findCard]);
+      findCard.flipped = true;
+    }
+    setCards([...cards]);
+  }
+
+  // use effect to handle compare flipped cards
+  useEffect(() => {
+    if (flipped.length === 2) {
+      setMoves(moves + 1);
+      setCanPlay(false);
+      const findCard1 = cards.find((card) => card.id === flipped[0].id);
+      const findCard2 = cards.find((card) => card.id === flipped[1].id);
+      if (flipped[0].name === flipped[1].name) {
+        findCard1.matched = true;
+        findCard2.matched = true;
+        setMatched([...matched, flipped[0], flipped[1]]);
+        setScore(score + 1);
+        setIsExploding(true);
+        setCards((prevCards) => [...prevCards]);
+      } else {
+        if (findCard1) {
+          setTimeout(() => {
+            findCard1.flipped = false;
+          }, 1000);
+        }
+        if (findCard2) {
+          setTimeout(() => {
+            findCard2.flipped = false;
+          }, 1000);
+        }
+        setIsExploding(false);
+
+        setCards((prevCards) => [...prevCards]);
+      }
+
+      if (moves === maxMoves) {
+        setGameOver(true);
+        setCanPlay(false);
+      }
+
+      setTimeout(() => {
+        setCanPlay(true);
+        setFlipped([]);
+      }, 1000);
+    }
+  }, [flipped]);
+
+  // type for mouse event
+  function handleMouseMove(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const card = e.target as HTMLDivElement;
+    const { x, y } = card.getBoundingClientRect();
+
+    const relativeX = e.clientX - x;
+    const relativeY = e.clientY - y;
+
+    card.style.setProperty("--x-cursor-position", String(relativeX));
+    card.style.setProperty("--y-cursor-position", String(relativeY));
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className={`flex flex-col gap-2 p-24 min-h-screen`}>
+      <div className="flex flex-col items-center gap-4 pb-10">
+        <h1 className="text-4xl text-[#5a5a87] font-bold relative">Score: {score}/{targertScore}<div className="absolute left-1/2 -translate-1/2">{isExploding && <ConfettiExplosion />}</div></h1>
+        {/* <h1 className="text-2xl text-[#5a5a87] font-bold text-center flex flex-col items-center justify-center relative">
+          Essais: {moves}/{maxMoves}
+          
+        </h1> */}
+      
+
+      </div>
+
+      {/* {gameOver && <h1 className="text-white">Game Over</h1>} */}
+      <div className="flex flex-wrap justify-center gap-8">
+        {cards.map((card) => (
+          <div
+            className={`flip-card ${card.flipped ? "flipped" : ""}`}
+            key={card.id}
+            onClick={() => handleClick(card.id)}
+            onMouseMove={(e) => handleMouseMove(e)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <div className="flip-card-inner">
+              <div
+                className={`flip-card-front rounded-lg flex items-center justify-center bg-white ${
+                  card.matched ? "border-8 !border-green-400" : ""
+                }`}
+              >
+                <div className="flex flex-col justify-center items-center">
+                  <p className="text-[#5a5a87] text-center">{card.name}</p>
+                  <img src={card.image} alt="" width={120} />
+                </div>
+              </div>
+              <div className="flip-card-back rounded-lg flex items-center justify-center border border-slate-500 bg-[#5a5a87]">
+                <Image src={Logo} alt="" width={120} />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
-  )
+  );
 }
